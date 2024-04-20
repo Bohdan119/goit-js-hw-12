463f8622fc93d6f4d1e156af32073a175e8a7452
Bohdan Matsko <bm01271970@gmail.com>
2024-04-20 17:42:25 +0200

    -0

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export async function fetchGallery(query, page) {
+  const BASE_URL = 'https://pixabay.com';
+  const END_POINT = '/api/';
+  const KEY_API = '?key=43226276-a07a0c17e428cfffb021b9b05';
+  const PARAMS = `&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15&page=${page}`;
+
+  const url = BASE_URL + END_POINT + KEY_API + PARAMS;
+
+  const res = await axios.get(url);
+  return res.data;
+}
